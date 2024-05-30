751500a001241a338c002d1026a332fa330f5684
talbinourelislem <talbinourelislem@gmail.com>
2024-05-30 23:12:34 +0100

    Add files via upload

---
+++ utils/HtmlScript.js
@@ -0,0 +1,68 @@
+const HtmlScript = `
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<base target="_top">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title>Quick Start - Leaflet</title>
+	
+	<link rel="shortcut icon" type="image/x-icon" href="docs/images/favicon.ico" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin=""/>
+
+	<link rel="stylesheet" href="leaflet-routing-machine.css" />
+	<script src="leaflet-routing-machine.js"></script>
+    <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
+
+	
+
+	<style>
+		html, body {
+			height: 100%;
+			margin: 0;
+		}
+		.leaflet-container {
+			height: 400px;
+			width: 600px;
+			max-width: 100%;
+			max-height: 100%;
+		}
+	</style>
+
+	
+</head>
+<body>
+
+
+<div id="map" style="width: 100%; height: 100%;"></div>
+<script>
+	
+	const map = L.map('map').setView([36.814016, 7.720433], 16);
+
+	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
+	const popup = L.popup();
+	function onMapClick(e) {
+		popup
+			.setLatLng(e.latlng)
+			.setContent('You clicked the map at ' + e.latlng.toString())
+			.openOn(map);
+	}
+	map.on('click', onMapClick);
+
+	
+
+</script>
+
+
+
+</body>
+</html>
+
+`;
+
+export default HtmlScript;
---
+++ utils/RequestGPSLocation.js
@@ -0,0 +1,5 @@
+/*eslint-disable prettier/prettier*/
+import {useEffect} from 'react';
+import Geolocation from '@react-native-community/geolocation';
+
+export default useLocationPermission;
